0ac8e7e0e5e7038a77b1ac206913854caa0de072
leandro <leandrofortunatojf@gmail.com>
2024-08-06 17:17:37 -0300

    coloca as coisas nos luugares corretos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
-      <Navbar />
-      {children}
-      <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,10 @@ import Footer from "@/components/footer";
 import Image from "next/image";
 
 export default function Home() {
-  return;
+  return(
+    <>
+    <Navbar />
+    <Footer />
+    </>
+  );
 }

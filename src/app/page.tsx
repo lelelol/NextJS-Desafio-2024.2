981915dec63ade6e60c26f015fe2bb7a3d7b59fb
leandro <leandrofortunatojf@gmail.com>
2024-08-06 17:48:15 -0300

    começa landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Image from "next/image";
+import Landing from "@/components/landing";
 
 export default function Home() {
   return(
     <>
     <Navbar />
+    <Landing />
     <Footer />
     </>
   );
---
+++ src/components/landing/index.tsx
@@ -0,0 +1,12 @@
+export default function Landing() {
+    return (
+      <div className="">
+        <div className="w-2/3 bg-cinza bg-opacity-45 flex justify-center content-center">
+            <p className="text-7xl">Encontre a sua próxima leitura.</p>
+        </div>
+    </div>
+           
+    );
+}
+
+        

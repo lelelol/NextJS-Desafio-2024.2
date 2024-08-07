60d1f3a63cdba6c0c36bc7ffd603a21d964118e0
leandro <leandrofortunatojf@gmail.com>
2024-08-07 13:05:56 -0300

    continua mais coisas

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,7 +1,10 @@
 export default function Landing() {
     return (
-      <div className="">
-        <div className="w-2/3 bg-cinza bg-opacity-45 flex justify-center content-center">
+      <div className="flex justify-center gap-28">
+        <div className="w-1/4 bg-cinza bg-opacity-45 flex justify-center content-center">
+            <p className="text-7xl pl-4">Encontre a sua próxima leitura.</p>
+        </div>
+        <div className="w-1/4 bg-cinza bg-opacity-45 flex justify-center content-center">
             <p className="text-7xl">Encontre a sua próxima leitura.</p>
         </div>
     </div>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,23 @@
 export default function Navbar() {
     return (
-      <div className="flex justify-center items-center h-12 bg-cinza w-full">
-        <div className="flex items-center justify-center space-x-5">
-          <a >Produtos</a>
-          <a >Contato</a>
-          <a className="pr-4 pl-4 text-2xl font-semibold">next reading</a>
-          <a >Login</a>
-          <a >Gerenciamento</a>
+      <div className="flex justify-center items-center h-12 bg-cinza w-screen">
+        <div className="flex items-center justify-end space-x-5 w-full">
+          <a>Produtos</a>
+          <a>Contato</a>
         </div>
-        <div className="absolute top-0 right-0 mt-1 mr-5 flex flex-col text-sm justify-center items-center">
+          <a className="w-fit text-2xl font-semibold whitespace-nowrap pl-4 pr-4">next reading</a>
+        <div className="w-full flex items-center justify-between space-x-5">
+          <div className="space-x-5">
+            <a >Login</a>
+            <a >Gerenciamento</a>
+          </div>
+          
+          <div className="flex flex-col text-sm justify-center items-center pr-4">
             <a>Carrinho</a>
             <a className="text-cinzatxt">R$ 0,00</a>
+          </div>
         </div>
+        
       </div> 
            
     );

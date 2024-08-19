a453efb4ec7cd5372d02088c32b2669922a2469b
leandro <leandrofortunatojf@gmail.com>
2024-08-19 14:56:33 -0300

    ajusta umas coistas la

--- src/components/carrossel/index.tsx
+++ src/components/carrossel/index.tsx
@@ -1,62 +1,81 @@
 "use client";
+
 import Image from "next/image";
 import Produto from "../Produto";
 import "@splidejs/splide/css";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import React from "react";
 
 export default function Carrossel() {
   return (
-    <Splide className="max-w-[800px]">
-      <SplideSlide>
-        <div className="flex bg-cinza bg-opacity-45 rounded-3xl p-4 mb-8">
-          <Image
-            className="rounded-lg"
-            src="/assets/ex.jpg"
-            width={140}
-            height={300}
-            alt="capa"
-          />
-          <div className="flex flex-col items-center justify-around pl-8">
-            <h2 className="font-semibold text-2xl">Bla bla bla</h2>
-            <p className="text-cinzatxt text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non
-              purus vitae nunc placerat sollicitudin. Mauris accumsan feugiat
-              eros eu accumsan. Maecenas scelerisque venenatis nulla eu
-              faucibus. Etiam consectetur nec metus a tempor. In pulvinar
-              vestibulum leo. Ut cursus elit a odio tincidunt rutrum.
-            </p>
+    <Splide
+      options={{ type: "loop", pagination: true }}
+      hasTrack={false}
+      className="max-w-[800px]"
+    >
+      <SplideTrack>
+        <SplideSlide>
+          <div className="flex bg-cinza bg-opacity-45 rounded-3xl p-4 mb-8">
+            <Image
+              className="rounded-lg"
+              src="/assets/ex.jpg"
+              width={140}
+              height={300}
+              alt="capa"
+            />
+            <div className="flex flex-col items-center justify-around pl-8">
+              <h2 className="font-semibold text-2xl">Bla bla bla</h2>
+              <p className="text-cinzatxt text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non
+                purus vitae nunc placerat sollicitudin. Mauris accumsan feugiat
+                eros eu accumsan. Maecenas scelerisque venenatis nulla eu
+                faucibus. Etiam consectetur nec metus a tempor. In pulvinar
+                vestibulum leo. Ut cursus elit a odio tincidunt rutrum.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-between">
-          <Produto />
-          <Produto />
-        </div>
-      </SplideSlide>
-      <SplideSlide>
-        <div className="flex w-full bg-cinza bg-opacity-45 rounded-3xl p-4 mb-8">
-          <Image
-            className="rounded-lg"
-            src="/assets/ex.jpg"
-            width={140}
-            height={300}
-            alt="capa"
-          />
-          <div className="flex flex-col items-center justify-around pl-8">
-            <h2 className="font-semibold text-2xl">Bla bla bla</h2>
-            <p className="text-cinzatxt text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non
-              purus vitae nunc placerat sollicitudin. Mauris accumsan feugiat
-              eros eu accumsan. Maecenas scelerisque venenatis nulla eu
-              faucibus. Etiam consectetur nec metus a tempor. In pulvinar
-              vestibulum leo. Ut cursus elit a odio tincidunt rutrum.
-            </p>
+          <div className="flex justify-between">
+            <Produto />
+            <Produto />
           </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className="flex w-full bg-cinza bg-opacity-45 rounded-3xl p-4 mb-8">
+            <Image
+              className="rounded-lg"
+              src="/assets/ex.jpg"
+              width={140}
+              height={300}
+              alt="capa"
+            />
+            <div className="flex flex-col items-center justify-around pl-8">
+              <h2 className="font-semibold text-2xl">Bla bla bla</h2>
+              <p className="text-cinzatxt text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam non
+                purus vitae nunc placerat sollicitudin. Mauris accumsan feugiat
+                eros eu accumsan. Maecenas scelerisque venenatis nulla eu
+                faucibus. Etiam consectetur nec metus a tempor. In pulvinar
+                vestibulum leo. Ut cursus elit a odio tincidunt rutrum.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <Produto />
+            <Produto />
+          </div>
+        </SplideSlide>
+      </SplideTrack>
+      <div className="splide__arrows flex justify-around">
+        <button className="splide__arrow--prev bg-cinza bg-opacity-45 rounded-3xl p-4 mt-3">
+          Ant
+        </button>
+        <div className="splide__progress">
+          <div className="splide__progress__bar" />
         </div>
-        <div className="flex justify-between">
-          <Produto />
-          <Produto />
-        </div>
-      </SplideSlide>
+        <button className="splide__arrow--next bg-cinza bg-opacity-45 rounded-3xl p-4 mt-3">
+          Prox
+        </button>
+      </div>
     </Splide>
   );
 }
--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -5,13 +5,11 @@ import Carrossel from "../carrossel";
 const OpenSans = Open_Sans({ subsets: ["latin"] });
 export default function Landing() {
   return (
-    <div className="flex justify-center bg-degrade">
+    <div id="divmae" className="flex justify-center bg-degrade">
       <div className="flex justify-center gap-10 m-3 max-w-[1250px]">
         <div className="flex gap-5 justify-center">
           <div className="p-10 bg-cinza bg-opacity-45 flex justify-center content-center rounded-bl-[90px] rounded-3xl">
-            <p
-              className={`${OpenSans.className} text-[90px] font-semibold -mt-6 leading-[1.1]`}
-            >
+            <p className="text-[90px] font-semibold -mt-6 leading-[1.1]">
               Encontre a sua próxima leitura.
             </p>
           </div>

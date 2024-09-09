0a0279b1b297ba27c9d0ff1881dbab391e9be6ca
lelelol <leandrofortunatojf@gmail.com>
2024-09-09 11:01:35 -0300

    arruma pagina de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -3,14 +3,14 @@ import Card from "@/components/card";
 export default function Contato() {
   return (
     <>
-      <div className="flex justify-center gap-[10%] m-[7%]">
+      <div className="flex justify-center md:gap-[10%] gap-[1%] m-[7%]">
         <Card
           title="Sobre nós"
           text="Nossa missão é proporcionar serviços e produtos de qualidade, buscando sempre a satisfação dos nossos clientes. Comprometemo-nos a agir com responsabilidade, integridade e inovação, garantindo um impacto positivo na vida das pessoas e na sociedade."
           id={0}
         />
         <Card
-  title="Entre em contato"
+  title="Contato"
   text={`Se você tiver alguma dúvida, sugestão ou apenas quiser conversar, estamos à disposição para ajudar.
   
 Entre em contato conosco pelo telefone (11) 1234-5678 ou envie um e-mail para contato@exemplo.com.
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -7,8 +7,10 @@ type CardProps = {
 export default function Card({ id, title, text }: CardProps) {
   return (
     <div className="whitespace-pre-line flex flex-col bg-cinza rounded-xl w-full sm:w-80 md:w-96 lg:w-1/4 xl:w-1/5 justify-center items-center gap-4 p-4">
-      <h1 className="font-semibold text-lg sm:text-xl md:text-2xl">{title}</h1>
-      <p className="text-sm sm:text-base md:text-lg text-cinzatxt text-center">
+      <h1 className="font-semibold text-base sm:text-lg md:text-xl lg:text-2xl">
+        {title}
+      </h1>
+      <p className="text-xs sm:text-sm md:text-base lg:text-lg text-cinzatxt text-center">
         {text}
       </p>
     </div>
